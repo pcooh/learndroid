9c6fbc7beebcac23eea1a48e3f5dd0368461f731
pcooh <pcooh3@gmail.com>
2015-07-30 21:10:43 +0900

    Storage Access Framework (SAF)

--- app/src/main/java/io/github/pcooh/learndroid/MainActivity.java
+++ app/src/main/java/io/github/pcooh/learndroid/MainActivity.java
@@ -8,7 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;//
 import android.util.Log;//
-
+import android.content.Intent;// ファイル ピッカー用
+import android.net.Uri;// ファイル ピッカーの戻り実装で使用
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private static final int OPEN_DOCUMENT_REQUEST = 1; //メニュー用
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -50,7 +52,34 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        // File ... メニューを選択したとき
+        else if (id == R.id.action_file) {
+            // ファイル ピッカーを表示
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.setType("*/*");
+            startActivityForResult(intent, OPEN_DOCUMENT_REQUEST);
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(resultCode != RESULT_OK)
+            return;
+        else{
+            switch (requestCode){
+                case OPEN_DOCUMENT_REQUEST:
+                    Uri url = data.getData();
+                    TextView textView = (TextView) findViewById(R.id.textView);
+                    Log.d(null, url.getPath());
+                    textView.setText(url.getPath());
+                    break;
+
+                default :
+                    break;
+            }
+        }
     }
 }
